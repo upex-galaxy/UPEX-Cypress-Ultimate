5197e3b0a2429c16e17c7748725f38a899f11eb2
verogeid <verogeid@gmail.com>
2025-04-09 11:58:50 +0200

    chore: (QA) Integrate cypressOptions into cypress.config.cron.ts to test configuration path

--- cypress.config.cron.ts
+++ cypress.config.cron.ts
@@ -1,6 +1,4 @@
-// cypress.config.cron.ts
 import { defineConfig } from 'cypress';
-import { cypressOptions } from './cypress/support/config/cypressOptions';
 import { envConfig } from './cypress/support/config/envConfig';
 import { setupNodeEvents } from './cypress/support/config/setupNodeEvents';
 
@@ -10,11 +8,24 @@ const secrets = ['ALWAYSDATA_USER', 'ALWAYSDATA_PASS'];
 const envConfigLoaded = envConfig(secrets);
 
 export default defineConfig({
-	...cypressOptions,
+	pageLoadTimeout: 20000,
+	projectId: '2pw67q', //? ID del proyecto CYPRESS-DEMO-CLOUD
+	viewportWidth: 1920,
+	viewportHeight: 1080,
+	downloadsFolder: 'cypress/downloads',
+	videosFolder: 'cypress/videos',
+	screenshotsFolder: 'cypress/screenshots',
+	screenshotOnRunFailure: true,
 	scrollBehavior: 'center',
+	retries: process.env.CI ? 3 : 0,
+	video: Boolean(process.env.CI),
+	watchForFileChanges: false,
+	chromeWebSecurity: false,
+	reporter: 'cypress-multi-reporters',
+	reporterOptions: {
+		configFile: 'cypress.reporter.chrome.json'
+	},
 	e2e: {
-		// baseUrl: 'https://demoqa.com',
-		// Glob pattern to determine what test files to load:
 		specPattern: ['cypress/Tests/**/*.cy.{js,jsx,ts,tsx}'],
 		excludeSpecPattern: ['cypress/Tests/**/*.example.cy.{js,ts}', 'docs/**/*.*'],
 		setupNodeEvents
